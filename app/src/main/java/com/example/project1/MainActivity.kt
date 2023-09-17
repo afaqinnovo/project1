aedc824a03558326a4a3993ebd7dc7ff82619f6e
Afaq Malik <mafaq6141@gmail.com>
2023-09-17 19:17:48 +0500

    Initial commit

---
+++ app/src/main/java/com/example/project1/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.project1
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import com.example.project1.ui.theme.Project1Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+}
